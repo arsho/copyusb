f2c599f3f130d2ec5ff3ef8a165d6402c9da259c
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2017-02-22 14:29:31 +0600

    updated version

--- copyusb/__init__.py
+++ copyusb/__init__.py
@@ -3,7 +3,7 @@
 # Description          : Copy connected USB media device to current script directory.
 #                        DO NOT MISUSE IT FOR STEALING DATA.                    
 # Date                 : 21/02/2017
-# Version              : 0.0.2
+# Version              : 0.0.3
 # Tested OS            : Ubuntu (16.04 LTS)
 # USB Drive Size (max) : 16GB
 
